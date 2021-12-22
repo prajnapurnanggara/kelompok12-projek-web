b10a814a52e1cba8d50664ff655e57a6f57b774a
prajnapurnanggara <gwmiilo@gmail.com>
2021-12-22 08:45:51 +0800

    page pesananku

--- src/views/Pesananku.vue
+++ src/views/Pesananku.vue
@@ -6,6 +6,70 @@
         <div class="col">
           <h2>Pesananku</h2>
         </div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col" width="2%">#</th>
+              <th scope="col"></th>
+              <th scope="col">Makanan</th>
+              <th scope="col">Harga</th>
+              <th scope="col">Jumlah</th>
+              <th scope="col">Total Harga</th>
+              <th scope="col">Catatan</th>
+              <th scope="col" width="2%"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td scope="col">1</td>
+              <td>
+                <img
+                  src="https://i.ytimg.com/vi/9qYtYm7HddM/maxresdefault.jpg"
+                  width="200"
+                  class="img-fluid shadow"
+                />
+              </td>
+              <td>Tipat Cantok</td>
+              <td>Rp 10.000</td>
+              <td>3</td>
+              <td>Rp 30.000</td>
+              <td>1 tanpa sayur kangkung, semuanya cabe 2</td>
+              <td></td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td><strong>Total : Rp 30.000</strong></td>
+              <td></td>
+            </tr>
+          </tfoot>
+        </table>
+
+        <div class="row justify-content-end">
+          <div class="col-3">
+            <form>
+              <div class="form-group mt-3">
+                <label><strong>Nama</strong></label>
+                <input type="Text" class="form-control mt-1" />
+              </div>
+              <div class="form-group mt-3">
+                <label><strong>Nomer Meja</strong></label>
+                <input type="number" class="form-control mt-1" />
+              </div>
+              <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button type="submit" class="btn btn-warning mt-4">
+                  Pesan Makanan
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -18,7 +82,7 @@ import Navbar from "@/components/Navbar.vue";
 export default {
   name: "Pesananku",
   components: {
-    Navbar
+    Navbar,
   },
 };
 </script>
